4864e2c8c7a47390a782fc885cf94462334bf73e
John Collins <johncollinsemail@gmail.com>
2026-02-24 20:07:22 -0500

    Brand browser tab title and favicon links

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,11 +1,11 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ $title ?? config('app.name') }}</title>
+<title>{{ $title ? $title.' · Modern Forestry Backstage' : 'Modern Forestry Backstage' }}</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="icon" href="{{ asset('favicon.svg') }}?v=mf1" type="image/svg+xml">
+<link rel="shortcut icon" href="{{ asset('favicon.svg') }}?v=mf1" type="image/svg+xml">
+<link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}?v=mf1">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600&family=fraunces:400,500,600,700" rel="stylesheet" />
