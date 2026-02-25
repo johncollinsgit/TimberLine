971061b17dfabe949f2464e328b4da19d531fcd4
John Collins <johncollinsemail@gmail.com>
2026-02-24 20:03:46 -0500

    Use branded tree logo in shared app header

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,17 +1,35 @@
 @props([
     'sidebar' => false,
+    'logoSrc' => null,
+    'logoAlt' => 'Modern Forestry Backstage',
 ])
+
+@php
+    $brandLogoSrc = $logoSrc ?: asset('favicon.svg');
+@endphp
 
 @if($sidebar)
     <flux:sidebar.brand name="Modern Forestry Backstage" {{ $attributes }}>
-        <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-            <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+        <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md overflow-hidden">
+            <img
+                src="{{ $brandLogoSrc }}"
+                alt="{{ $logoAlt }}"
+                class="block size-8 object-contain"
+                loading="eager"
+                decoding="async"
+            />
         </x-slot>
     </flux:sidebar.brand>
 @else
     <flux:brand name="Modern Forestry Backstage" {{ $attributes }}>
-        <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-            <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+        <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md overflow-hidden">
+            <img
+                src="{{ $brandLogoSrc }}"
+                alt="{{ $logoAlt }}"
+                class="block size-8 object-contain"
+                loading="eager"
+                decoding="async"
+            />
         </x-slot>
     </flux:brand>
 @endif
