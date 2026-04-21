deaf30e0662703f181012cb31967a944b1c7060c
John Collins <johncollinsemail@gmail.com>
2026-04-20 22:17:19 -0400

    Align Shopify OAuth default scopes with app config

--- config/services.php
+++ config/services.php
@@ -144,7 +144,7 @@ return [
 
         'scopes' => env(
             'SHOPIFY_SCOPES',
-            'read_products,read_orders,read_all_orders,read_reports,read_analytics,read_customers,write_customers,read_discounts,write_discounts,read_webhooks,write_webhooks,read_pixels,write_pixels,read_customer_events'
+            'read_products,read_orders,read_all_orders,read_reports,read_analytics,read_customers,write_customers,read_discounts,write_discounts,read_pixels,write_pixels,read_customer_events'
         ),
         // Stores used by default when commands run without an explicit store argument.
         'active_store_keys' => env('SHOPIFY_ACTIVE_STORE_KEYS', 'retail'),
